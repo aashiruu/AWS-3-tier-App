9b79b65fd988358298cc061eb0a2efa2f0a73b5d
famous <138824251+aashiruu@users.noreply.github.com>
2025-08-30 23:39:43 +0100

    Create providers.tf

---
+++ terraform/providers.tf
@@ -0,0 +1,18 @@
+
+```hcl
+# Specify the required Terraform version and providers
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.aws_region
+}
